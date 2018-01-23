49165a737fd044b4601a9c8c9ad23186f0b9139c
Isabelle PORSENNA <iporsenn@e1r12p15.42.fr>
2018-01-23 17:17:11 +0100

    buh 2

--- get_next_line.c
+++ get_next_line.c
@@ -25,40 +25,45 @@ int		get_next_line(int fd, char **line)
 	if (!(*line = ft_strnew(BUFF_SIZE)))
 		return (0);
 	if (!(tmp = ft_strnew(BUFF_SIZE)))
+		return (0);
 	ret = 0;
 	reste = (reste != NULL) ? reste : NULL;
-	if (reste != NULL)
-	{
-		ft_putendl("if reste");
-		i = -1;
-		while (reste[++i] == '\n')
-			;
-		tmp = ft_strsub(reste, i, ft_strlen(reste));
-		ft_putstr("tmp : ");
-		ft_putstr(tmp);
-		ft_memdel((void**)&reste);
-		i = -1;
-		while (tmp[++i] != '\n')
-			;
-		reste = ft_strsub(tmp, i, (size_t)ft_strlen(tmp));
-		ft_putstr("reste : ");
-		ft_putnbr(ft_strlen(reste));
-		ft_putstr(reste);
-	}
+	// if (reste != NULL)
+	// {
+	// 	ft_putendl("if reste");
+	// 	i = -1;
+	// 	while (reste[++i] == '\n')
+	// 		;
+	// 	tmp = ft_strsub(reste, i, ft_strlen(reste));
+	// 	ft_putstr("tmp : ");
+	// 	ft_putstr(tmp);
+	// 	ft_memdel((void**)&reste);
+	// 	i = -1;
+	// 	while (tmp[++i] != '\n')
+	// 		;
+	// 	reste = ft_strsub(tmp, i, (size_t)ft_strlen(tmp));
+	// 	ft_putstr("reste : ");
+	// 	ft_putnbr(ft_strlen(reste));
+	// 	ft_putstr(reste);
+	// 	if (ft_strlen(reste) == 0)
+	// 		ft_memdel((void**)&reste);
+	// }
 	while ((ret = read(fd, buff, BUFF_SIZE)) >= 0 && !ft_strchr(tmp, '\n'))
 	{
 		buff[ret] = '\0';
-		if (ft_strchr(buff, '\n') == NULL)
-			ft_strcat(tmp, buff);
-		else
+		if (ft_strchr(buff, '\n') != NULL)
 		{
-			ft_memccpy(tmp, buff, '\n', BUFF_SIZE);
+			tmp = ft_strjoin(tmp, buff);
 			i = -1;
 			while (buff[++i] != '\n')
 				;
 			reste = ft_strsub(buff, i, (size_t)BUFF_SIZE);
 		}
+		else
+			ft_strcat(tmp, buff);
 	}
+	ft_putstr("tmp = ");
+	ft_putstr(tmp);
 	ft_memccpy(*line, tmp, '\n', ft_strlen(tmp));
 	ft_memdel((void **)&tmp);
 	if (ret > 0 || reste != NULL)
